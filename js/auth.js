955e0f2c81c6327f662d16f2dc3fa5dad487e350
CobraCom-ux <cobracommunicationllc@gmail.com>
2026-05-29 08:15:54 -0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,31 @@
+import { auth } from "./firebase-config.js";
 
+import {
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+window.login = async function () {
+
+  const email = document.getElementById("email").value;
+
+  const password = document.getElementById("password").value;
+
+  try {
+
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
+    alert("Login Successful");
+
+    window.location.href = "dashboard.html";
+
+  } catch (error) {
+
+    alert(error.message);
+
+  }
+
+}
